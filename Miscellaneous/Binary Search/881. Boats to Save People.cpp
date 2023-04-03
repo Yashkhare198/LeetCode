7f12f9463a4f36239029a5448724963261af9676
Yashkhare198 <64392890+Yashkhare198@users.noreply.github.com>
2023-04-03 07:26:34 +0530

    Create 881. Boats to Save People.cpp
    Binary Seach

---
+++ Miscellaneous/Binary Search/881. Boats to Save People.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+
+        sort(people.begin(),people.end());
+
+        int n = people.size();
+
+        int s=0;
+        int e=n-1;
+        int boats = 0;
+
+        while(s<=e){
+            if(people[s]+people[e] <= limit){
+                s++;
+                e--;
+            }
+            else if(people[s]+people[e]>limit){
+                e--;
+            }
+            boats++;
+        }
+        return boats;
+        
+    }
+};

3e977099f759b6e322d64a2b102cb10e3992d86a
Yashkhare198 <64392890+Yashkhare198@users.noreply.github.com>
2023-04-24 18:20:40 +0530

    Create 1046. Last Stone Weight_24_04_23.cpp
    Using Priority queue

---
+++ POTD/1046. Last Stone Weight_24_04_23.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+
+        priority_queue<int> pq(stones.begin(),stones.end());
+
+        while(pq.size()>1){
+            int x =pq.top();
+            pq.pop();
+
+            int y =pq.top();
+            pq.pop();
+
+            if(x != y){
+                pq.push(x-y);
+            }
+        }
+
+        return !pq.empty()? pq.top(): 0;
+
+
+        
+    }
+};

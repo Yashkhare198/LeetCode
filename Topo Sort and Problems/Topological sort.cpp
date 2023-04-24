29aecf2a70169ad65be096de8349735eeac8bfbe
Yashkhare198 <64392890+Yashkhare198@users.noreply.github.com>
2023-04-24 20:02:30 +0530

    Update Topological sort.cpp
    Using DFS with stack & using BFS with indegree concept

--- Topo Sort and Problems/Topological sort.cpp
+++ Topo Sort and Problems/Topological sort.cpp
@@ -1,3 +1,5 @@
+#Approach 1(Using DFS)
+
 class Solution
 {
 	public:
@@ -32,5 +34,47 @@ class Solution
 	        st.pop();
 	    }
 	    return ans;
+	}
+};
+
+//Approach 2(Using BFS)
+
+class Solution
+{
+	public:
+	
+
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	   vector<int>indegree(V,0);
+	   
+	   for(int i=0;i<V;i++){
+	       for(auto x:adj[i]){
+	           indegree[x]++;
+	       }
+	   }
+	   queue<int>q;
+	   for(int i=0;i<V;i++){
+	       if(indegree[i] == 0){
+	           q.push(i);
+	       }
+	   }
+	   vector<int>topo;
+	   while(!q.empty()){
+	       int node = q.front();
+	       q.pop();
+	       topo.push_back(node);
+	       
+	       for(auto x:adj[node]){
+	           indegree[x]--;
+	           if(indegree[x]==0){
+	               q.push(x);
+	           }
+	       }
+	   }
+	   return topo;
+	   
 	}
 };

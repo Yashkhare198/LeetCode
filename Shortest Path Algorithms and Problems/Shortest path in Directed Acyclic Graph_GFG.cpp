b3ce8fad04a56cc45da4ecea31ac2e8fa98df57f
Yashkhare198 <64392890+Yashkhare198@users.noreply.github.com>
2023-04-27 18:54:49 +0530

    Update Shortest path in Directed Acyclic Graph_GFG.cpp
    2 approaches

--- Shortest Path Algorithms and Problems/Shortest path in Directed Acyclic Graph_GFG.cpp
+++ Shortest Path Algorithms and Problems/Shortest path in Directed Acyclic Graph_GFG.cpp
@@ -42,3 +42,68 @@ class Solution {
         return dis;
     }
 };
+
+
+// Approach 2(Using Toposort DFS)
+
+  void toposort(int node , vector<int>&vis ,vector<vector<pair<int,int>>>&adj ,stack<int>&st){
+      
+      vis[node] = 1;
+      
+      for(auto x: adj[node]){
+          int v = x.first;
+        //   int wt = x.second;
+          if(!vis[v]){
+              toposort(v,vis,adj,st);
+          }
+      }
+      st.push(node);
+  }
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+        // code here
+          
+          vector<vector<pair<int,int>>>adj(N);
+          
+          for(int i = 0; i<M ;i++)
+          {
+              int u = edges[i][0];
+              int v = edges[i][1];
+              int wt = edges[i][2];
+              
+              adj[u].push_back({v,wt});
+          }
+          
+          vector<int>vis(N,0);
+          stack<int>st;
+          
+          for(int i=0;i<N;i++){
+              if(!vis[i]){
+                  toposort(i,vis,adj,st);
+              }
+          }
+          vector<int>dis(N,1e9);
+          dis[0] = 0;
+          while(!st.empty())
+          {
+              int node = st.top();
+              st.pop();
+              
+              for(auto it: adj[node]){
+                  int v = it.first;
+                  int wt = it.second;
+                  
+                  if(dis[node]+wt < dis[v] ){
+                      dis[v] =dis[node] + wt;
+                  }
+              }
+              
+          }
+          
+          for(int i = 0 ; i<N ;i++){
+              if(dis[i] == 1e9){
+                  dis[i] = -1;
+              }
+          }
+          return dis;
+    }
+};

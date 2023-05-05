2d88c02abe8cab74e5bb12baf3122aa0a4f6216b
Yashkhare198 <64392890+Yashkhare198@users.noreply.github.com>
2023-05-05 08:35:18 +0530

    Create 649. Dota2 Senate.cpp
    Using 2 queue for handling the two paryies & i+nfor handling round table

---
+++ POTD/649. Dota2 Senate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        
+        queue<int> qr,qd;
+        int n = senate.size();
+        for(int i = 0; i<n ;i++){
+            if(senate[i] == 'R'){
+                qr.push(i);
+            }
+            else{
+                qd.push(i);
+            }
+        }
+
+        while(!qr.empty() && !qd.empty()){
+            int r_id =qr.front(),q_id =qd.front();
+            qr.pop(); qd.pop();
+
+            (r_id<q_id)?qr.push(r_id+n):qd.push(q_id+n);
+
+        }
+        return(qr.size()>qd.size())?"Radiant":"Dire";
+    }
+};

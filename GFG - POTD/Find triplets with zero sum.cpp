2849b8b41415aae1ae8004e01d1cc7c9af7b287f
Yash Khare <64392890+Yashkhare198@users.noreply.github.com>
2023-07-08 08:19:49 +0530

    Create Find triplets with zero sum.cpp
    for loop + binary search

---
+++ GFG - POTD/Find triplets with zero sum.cpp
@@ -0,0 +1,28 @@
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    { 
+        //Your code here
+        sort(arr,arr+n);
+        
+        for(int  i = 0; i<n-2; i++){
+          int low = i+1;
+          int high = n-1;
+          
+          while(low<high){
+              
+              if(arr[i]+arr[low]+arr[high] == 0) return true;
+              
+              else if(arr[i]+arr[low]+arr[high] < 0)
+              {
+                  low++;
+              }
+              else{
+                  high--;
+              }
+          }
+        }
+        return false;
+    }
+};

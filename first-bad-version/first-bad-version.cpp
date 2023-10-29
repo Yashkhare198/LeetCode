6483742adc770ec698f03359cfb137049995edce
Yash Khare <64392890+Yashkhare198@users.noreply.github.com>
2023-10-29 23:44:01 +0530

    Time: 0 ms, Memory: 6.3 MB - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -8,20 +8,20 @@ public:
         int s = 0;
         int e = n;
         
-        while(s+1<e)
+        while(s<e)
         {
             int mid = s+(e-s)/2;
             
-            if(isBadVersion(mid)==false)
+            if(isBadVersion(mid)==true)
             {
-                s = mid;
+                e = mid;
             }
             else
             {
-                e=mid;
+                s=mid+1;
             }
         }
-        return e;
+        return s;
        
         
     }
